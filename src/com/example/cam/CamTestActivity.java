47a2957cec51daea99c49aa363a8d1cb80c614fe
Alaeddin Nassani <anassani@jadeworld.com>
2015-11-26 23:50:35 +1300

    show button to take photo + change the picture size

--- src/com/example/cam/CamTestActivity.java
+++ src/com/example/cam/CamTestActivity.java
@@ -13,8 +13,16 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.hardware.Camera;
+import android.hardware.Camera.AutoFocusCallback;
+import android.hardware.Camera.Parameters;
 import android.hardware.Camera.PictureCallback;
 import android.hardware.Camera.ShutterCallback;
+import android.hardware.Camera.Size;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorListener;
+import android.hardware.SensorManager;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -29,15 +37,20 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class CamTestActivity extends Activity {
+public class CamTestActivity extends Activity implements SensorEventListener {
 	private static final String TAG = "CamTestActivity";
 	Preview preview;
 	Button buttonClick;
 	Camera camera;
 	Activity act;
 	Context ctx;
+	TextView text;
+
+	private SensorManager mSensorManager;
+	private Sensor mOrientation;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -49,90 +62,153 @@ public class CamTestActivity extends Activity {
 
 		setContentView(R.layout.main);
 
-		preview = new Preview(this, (SurfaceView)findViewById(R.id.surfaceView));
+		mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+		mOrientation = mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
+
+		text = (TextView) findViewById(R.id.txtStatus);
+		preview = new Preview(this, (SurfaceView) findViewById(R.id.surfaceView));
 		preview.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
 		((FrameLayout) findViewById(R.id.layout)).addView(preview);
 		preview.setKeepScreenOn(true);
 
-		preview.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
-				camera.takePicture(shutterCallback, rawCallback, jpegCallback);
-			}
-		});
+		// preview.setOnClickListener(new OnClickListener() {
+		//
+		// @Override
+		// public void onClick(View arg0) {
+		// Log.d(TAG, "preview.setOnClickListener");
+		// // camera.takePicture(shutterCallback, rawCallback,
+		// // jpegCallback);
+		// }
+		// });
 
 		Toast.makeText(ctx, getString(R.string.take_photo_help), Toast.LENGTH_LONG).show();
 
-		//		buttonClick = (Button) findViewById(R.id.btnCapture);
-		//		
-		//		buttonClick.setOnClickListener(new OnClickListener() {
-		//			public void onClick(View v) {
-		////				preview.camera.takePicture(shutterCallback, rawCallback, jpegCallback);
-		//				camera.takePicture(shutterCallback, rawCallback, jpegCallback);
-		//			}
-		//		});
-		//		
-		//		buttonClick.setOnLongClickListener(new OnLongClickListener(){
-		//			@Override
-		//			public boolean onLongClick(View arg0) {
-		//				camera.autoFocus(new AutoFocusCallback(){
-		//					@Override
-		//					public void onAutoFocus(boolean arg0, Camera arg1) {
-		//						//camera.takePicture(shutterCallback, rawCallback, jpegCallback);
-		//					}
-		//				});
-		//				return true;
-		//			}
-		//		});
+		buttonClick = (Button) findViewById(R.id.btnCapture);
+
+		buttonClick.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				// preview.camera.takePicture(shutterCallback, rawCallback,
+				// jpegCallback);
+				Log.d(TAG, "setOnClickListener");
+
+//				camera.stopPreview();
+//				camera.takePicture(shutterCallback, rawCallback, jpegCallback);
+								
+				camera.takePicture(null, null, jpegCallback);
+
+			}
+		});
+
+		buttonClick.setOnLongClickListener(new OnLongClickListener() {
+			@Override
+			public boolean onLongClick(View arg0) {
+
+				camera.autoFocus(new AutoFocusCallback() {
+					@Override
+					public void onAutoFocus(boolean arg0, Camera arg1) {
+						// camera.takePicture(shutterCallback, rawCallback,
+						// jpegCallback);
+					}
+				});
+
+				return true;
+			}
+		});
+
 	}
 
 	@Override
 	protected void onResume() {
 		super.onResume();
 		int numCams = Camera.getNumberOfCameras();
-		if(numCams > 0){
-			try{
+		if (numCams > 0) {
+			try {
 				camera = Camera.open(0);
+
+				Parameters params = camera.getParameters();
+				logCameraParameters(params);
+						
+				params.setPictureSize(640, 480);				
+				camera.setParameters(params);
+				
+				logCameraParameters(params);
+				
 				camera.startPreview();
 				preview.setCamera(camera);
-			} catch (RuntimeException ex){
+			} catch (RuntimeException ex) {
 				Toast.makeText(ctx, getString(R.string.camera_not_found), Toast.LENGTH_LONG).show();
 			}
 		}
+
+		mSensorManager.registerListener(this, mOrientation, SensorManager.SENSOR_DELAY_NORMAL);
+	}
+
+	private void logCameraParameters(Parameters params) {
+		Size s = params.getPictureSize();
+		Log.d(TAG, String.format("width: %s, height: %s", s.width, s.height));
 	}
 
 	@Override
 	protected void onPause() {
-		if(camera != null) {
+		if (camera != null) {
 			camera.stopPreview();
 			preview.setCamera(null);
 			camera.release();
 			camera = null;
 		}
 		super.onPause();
+
+		mSensorManager.unregisterListener(this);
+	}
+
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+		// Do something here if sensor accuracy changes.
+		// You must implement this callback in your code.
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		float azimuth_angle = event.values[0];
+		float pitch_angle = event.values[1];
+		float roll_angle = event.values[2];
+
+		text.setText(String.format("rotation %s,%s,%s", azimuth_angle, pitch_angle, roll_angle));
 	}
 
 	private void resetCam() {
 		camera.startPreview();
 		preview.setCamera(camera);
+
 	}
 
 	private void refreshGallery(File file) {
-		Intent mediaScanIntent = new Intent( Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+		Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
 		mediaScanIntent.setData(Uri.fromFile(file));
 		sendBroadcast(mediaScanIntent);
 	}
 
 	ShutterCallback shutterCallback = new ShutterCallback() {
 		public void onShutter() {
-			//			 Log.d(TAG, "onShutter'd");
+			Log.d(TAG, "onShutter'd");
 		}
 	};
 
 	PictureCallback rawCallback = new PictureCallback() {
 		public void onPictureTaken(byte[] data, Camera camera) {
-			//			 Log.d(TAG, "onPictureTaken - raw");
+			Log.d(TAG, "onPictureTaken - raw");
+		}
+	};
+
+	PictureCallback postviewCallback = new PictureCallback() {
+		public void onPictureTaken(byte[] data, Camera camera) {
+			Log.d(TAG, "taking picture - postview");
+
+			// new SaveImageTask().execute(data);
+			// resetCam();
+			//
+			// Log.d(TAG, "onPictureTaken - postview");
 		}
 	};
 
@@ -148,13 +224,15 @@ public class CamTestActivity extends Activity {
 
 		@Override
 		protected Void doInBackground(byte[]... data) {
+			Log.d(TAG, "saving image..");
+
 			FileOutputStream outStream = null;
 
 			// Write to SD Card
 			try {
 				File sdCard = Environment.getExternalStorageDirectory();
-				File dir = new File (sdCard.getAbsolutePath() + "/camtest");
-				dir.mkdirs();				
+				File dir = new File(sdCard.getAbsolutePath() + "/camtest");
+				dir.mkdirs();
 
 				String fileName = String.format("%d.jpg", System.currentTimeMillis());
 				File outFile = new File(dir, fileName);
@@ -167,6 +245,9 @@ public class CamTestActivity extends Activity {
 				Log.d(TAG, "onPictureTaken - wrote bytes: " + data.length + " to " + outFile.getAbsolutePath());
 
 				refreshGallery(outFile);
+
+				Log.d(TAG, "image saved");
+
 			} catch (FileNotFoundException e) {
 				e.printStackTrace();
 			} catch (IOException e) {
@@ -178,5 +259,3 @@ public class CamTestActivity extends Activity {
 
 	}
 }
-
-
